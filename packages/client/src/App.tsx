9ae6daaf657c6a1f2b037b289e1176ecbee01a38
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-12-10 14:22:02 +0000

    calling the backend

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,25 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { Test } from './components/Test';
+import ChatBot from './components/ChatBot';
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('api/hello')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-    return () => {};
-  }, []);
-
   return (
     <div>
-      <div className="bg-rose-300 p-5 text-2xl text-center m-10 rounded-lg animate-bounce">
-        <p>{message}</p>
-        <Button>Click me</Button>
-      </div>
-      <div className="flex justify-center">
-        <Test />
-      </div>
+      <ChatBot />
     </div>
   );
 }
---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,46 @@
+import { FaArrowUp } from 'react-icons/fa';
+import { Button } from './ui/button';
+import type { KeyboardEvent } from 'react';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  prompt: string;
+};
+
+const ChatBot = () => {
+  const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+  const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault(); // prevents new line when reset
+      handleSubmit(onSubmit)();
+    }
+  };
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      onKeyDown={onKeyDown}
+      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+    >
+      <textarea
+        className="w-full border-0 focus:outline-0 resize-none"
+        placeholder="Ask anything"
+        maxLength={1000}
+        {...register('prompt', {
+          required: true,
+          validate: (data) => data.trim().length > 0,
+        })}
+      />
+      <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
+        <FaArrowUp />
+      </Button>
+    </form>
+  );
+};
+
+export default ChatBot;
